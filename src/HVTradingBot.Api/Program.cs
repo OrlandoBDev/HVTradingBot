d8971db31bd7ff6e6f6fa78f07d99bbf625403fe
Orlando Blackwood <Blackwood0110@gmail.com>
2026-09-24 13:02:57 -0500

    Wire validated options and infrastructure

--- src/HVTradingBot.Api/Program.cs
+++ src/HVTradingBot.Api/Program.cs
@@ -1,12 +1,26 @@
-using HVTradingBot.Application.Abstractions;
-using HVTradingBot.Infrastructure.Brokers;
+using HVTradingBot.Application.Configuration;
+using HVTradingBot.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services
+    .AddOptions<TradingOptions>()
+    .Bind(builder.Configuration.GetSection(TradingOptions.SectionName))
+    .ValidateOnStart();
+
+builder.Services
+    .AddOptions<RiskOptions>()
+    .Bind(builder.Configuration.GetSection(RiskOptions.SectionName))
+    .Validate(x => x.MaxRiskPerTradePercent > 0 && x.MaxRiskPerTradePercent <= 5,
+        "MaxRiskPerTradePercent must be greater than 0 and no more than 5.")
+    .Validate(x => x.MaxDailyLossPercent > 0, "MaxDailyLossPercent must be greater than 0.")
+    .Validate(x => x.MaxOpenPositions > 0, "MaxOpenPositions must be greater than 0.")
+    .Validate(x => x.MinimumRiskReward > 0, "MinimumRiskReward must be greater than 0.")
+    .ValidateOnStart();
+
 builder.Services.AddOpenApi();
 builder.Services.AddHealthChecks();
-builder.Services.AddSingleton<PaperTradingBroker>();
-builder.Services.AddSingleton<IBroker>(sp => sp.GetRequiredService<PaperTradingBroker>());
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -17,7 +31,7 @@ app.MapGet("/", () => Results.Ok(new
     application = "HVTradingBot",
     version = "0.1.0",
     tradingMode = "PAPER",
-    status = "Foundation"
+    status = "MVP Development"
 }));
 
 app.Run();
